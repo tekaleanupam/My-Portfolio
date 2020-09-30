c84c047573c8206e76c71c308ae6470d8bf5487c
Anupam Tekale <72149411+tekaleanupam@users.noreply.github.com>
2020-09-30 20:51:21 +0530

    Add files via upload

---
+++ lib/footer.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:my_portfolio/header.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import 'colors.dart';
+
+class FooterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return VStack(
+      [
+        VxDevice(
+          mobile: VStack(
+            [
+              "Got a project?\nLet's talk.".text.center.white.xl2.make(),
+              10.heightBox,
+              "anuragtekale300@gmail.com"
+                  .text
+                  .color(Coolors.accentColor)
+                  .semiBold
+                  .make().shimmer()
+                  .box
+                  .border(color: Coolors.accentColor)
+                  .p16
+                  .rounded
+                  .make().shimmer(),
+            ],
+            crossAlignment: CrossAxisAlignment.center,
+          ),
+          web: HStack(
+            [
+              "Got a project?\nLet's talk.".text.center.white.xl2.make(),
+              10.widthBox,
+              "anuragtekale300@gmail.com"
+                  .text
+                  .color(Coolors.accentColor)
+                  .semiBold
+                  .make().shimmer()
+                  .box
+                  .border(color: Coolors.accentColor)
+                  .p16
+                  .rounded
+                  .make().shimmer(),
+            ],
+            alignment: MainAxisAlignment.spaceAround,
+          ).w(context.safePercentWidth * 70).scale150().p16(),
+        ),
+        50.heightBox,
+        CustomAppBar(),
+        10.heightBox,
+        "Thanks for scrolling, ".richText.semiBold.white.withTextSpanChildren(
+            ["that's all folks.".textSpan.gray500.make()]).make(),
+        10.heightBox,
+        SocialAccounts(),
+        30.heightBox,
+        [
+          "Anurag Tekale".text.red500.make(),
+          10.widthBox,
+          Icon(
+            AntDesign.heart,
+            color: Vx.red500,
+            size: 14,
+          )
+        ].hStack(crossAlignment: CrossAxisAlignment.center)
+      ],
+      crossAlignment: CrossAxisAlignment.center,
+    ).wFull(context).p16();
+  }
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import 'colors.dart';
+import 'footer.dart';
+import 'header.dart';
+import 'middlepart.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Coolors.primaryColor,
+      child: VStack([
+        HeaderScreen(),
+        if (context.isMobile) IntroductionWidget().p16(),
+        MiddleScreen(),
+        FooterScreen(),
+      ]).scrollVertical(),
+    );
+  }
+}
